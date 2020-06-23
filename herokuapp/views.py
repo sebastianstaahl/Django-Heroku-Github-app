2aa99db34588551083700099f4450e78c3f571bd
sebastianstaahl <basse_9_3@hotmail.com>
2020-06-23 21:49:44 +0200

    minor

--- herokuapp/views.py
+++ herokuapp/views.py
@@ -116,7 +116,7 @@ def webbhooks(request):
         payload.save()
         context = {}
         context['payload'] = payload.payload
-    return HttpResponse('<p>success</p>')
+    return True
 
 
 def listofhooks(response):
